c7f2ef77f7694fb5ee3067e51dfd26ff8bd0159b
Iamvictorsam <iamvictorsam@gmail.com>
2022-11-08 15:08:47 +0100

    update

---
+++ lib/helper/constants.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+const kGrey = Color(0xff2C3333);
+const kPrimary = Color(0xff395B64);
+const kLight = Color(0xffA5C9CA);
+const kVeryLight = Color(0xffE7F6F2);
+const kWhite = Colors.white;
+const msgGrey = Color(0xffF5F4F4);
+
+const lightTeal = Color(0xffA7FFED);
+const teal = Color(0xff006C59);
+
+const headStyle = TextStyle(
+  color: kGrey,
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+);
+
+final kGreenLight = const Color(0xFF009933).withOpacity(0.5);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF07498B), Color(0xFF042c54)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+
+final headingStyle = TextStyle(
+  fontSize: 28.sp,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  height: 1.5,
+);
+const divide = Divider(
+  color: kGrey,
+  // height: 1,
+  thickness: 1,
+);
+const defaultDuration = Duration(milliseconds: 250);
+final kPadding = 20.w;
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kFnameNullError = "Please Enter your First name";
+const String kLnameNullError = "Please Enter your Last name";
+const String kUsernameNullError = "Please Enter your Username";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    // fontFamily: 'Montserrat',
+    // appBarTheme: appBarTheme(),
+    // textTheme: textTheme(),
+    useMaterial3: true,
+
+    inputDecorationTheme: inputDecorationTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: const BorderSide(color: kGrey),
+    gapPadding: 10,
+  );
+  return InputDecorationTheme(
+    labelStyle:
+        TextStyle(fontSize: 12.sp, color: kGrey, fontWeight: FontWeight.w600),
+    contentPadding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: enabledInputBorder(),
+    border: outlineInputBorder,
+  );
+}
+
+OutlineInputBorder enabledInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(6),
+    borderSide: const BorderSide(color: kPrimary),
+  );
+}
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(6),
+    borderSide: const BorderSide(color: kGrey),
+  );
+}
+
+const OutlineInputBorder koutlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(50)),
+    gapPadding: 10,
+    borderSide: BorderSide(color: kGrey, width: 1, style: BorderStyle.solid));
+
+const kContainerBackground = Color(0xff657989);
+const kWalletContainerBackgroundG = Color(0xffABA7A7);
+const kContainerBackgroundW = Color(0xffF2F2F2);
+const knotificationButtonColor = Color(0xffC2E0CC);
+const kTextSyleW = TextStyle(color: Colors.white, fontSize: 14);
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,7 @@
+import 'package:doti_app/helper/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,7 +11,123 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         body: SafeArea(
             child: SingleChildScrollView(
-      child: Column(children: const []),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(children: [
+          ListTile(
+            title: Text(
+              'Welcome Back,',
+              style: TextStyle(fontSize: 16.sp),
+            ),
+            subtitle: Text(
+              'Sam,',
+              style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+            ),
+            trailing: Container(
+              decoration: BoxDecoration(
+                  color: lightTeal, borderRadius: BorderRadius.circular(8)),
+              padding: const EdgeInsets.all(8),
+              child: SvgPicture.asset('assets/icon/notification.svg'),
+            ),
+          ),
+          Container(
+              decoration: BoxDecoration(
+                  color: teal, borderRadius: BorderRadius.circular(8)),
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Text(
+                    'Recycling of of waste\ncan save\nthe environment\nfrom hazard ',
+                    style: TextStyle(
+                        fontSize: 22.sp,
+                        color: kWhite,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'So recycle your trash today',
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: kWhite,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: teal, borderRadius: BorderRadius.circular(8)),
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Waste Pickup',
+                          style: TextStyle(
+                            fontSize: 18.sp,
+                            color: const Color(0xff4DAB96),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Set a spefic location\nfor waste pickup',
+                              style: TextStyle(
+                                fontSize: 18.sp,
+                                color: const Color(0xff4DAB96),
+                              ),
+                            ),
+                            SvgPicture.asset("assets/recycle.svg"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Services'),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text('see all'),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: teal,
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            children: const [
+              CustomCard(child: SizedBox()),
+            ],
+          )
+        ]),
+      ),
     )));
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  const CustomCard(
+      {super.key, required this.child, this.padding = 8, this.radius = 8});
+
+  final Widget child;
+  final double padding, radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: teal, borderRadius: BorderRadius.circular(radius)),
+      padding: EdgeInsets.all(padding),
+      child: child,
+    );
   }
 }
