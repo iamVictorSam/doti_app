ae9348c6a5741f92ecb6bccf857b00ac3c594bac
Iamvictorsam <iamvictorsam@gmail.com>
2022-11-11 15:18:49 +0100

    card

--- lib/card/card.dart
+++ lib/card/card.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+import '../helper/constants.dart';
 
 class CardHistory extends StatelessWidget {
   const CardHistory({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                const CardTile(),
+                SizedBox(
+                  height: 25.h,
+                ),
+                SizedBox(
+                  width: Get.width,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -20,8 +43,55 @@ class CardTile extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         border: Border.all(
-          color: const Color.fromRGBO(0, 0, 0, 0.1),
+          color: const Color.fromRGBO(4, 128, 109, 0.24),
         ),
+      ),
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/icon/Payment.svg'),
+          Column(
+            children: [
+              Text(
+                'Visa ending in 1234',
+                style: TextStyle(
+                    color: const Color(
+                      0xff344054,
+                    ),
+                    fontSize: 16.sp),
+              ),
+              Text(
+                'Expiry 06/2024',
+                style: TextStyle(
+                    color: const Color(
+                      0xff667085,
+                    ),
+                    fontSize: 16.sp),
+              ),
+              SizedBox(height: 5.h),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Set as default',
+                    style: TextStyle(
+                        color: const Color(
+                          0xff667085,
+                        ),
+                        fontSize: 16.sp),
+                  ),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  Text(
+                    'Edit',
+                    style: TextStyle(color: teal, fontSize: 16.sp),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Radio(value: false, groupValue: false, onChanged: (vaule) {})
+        ],
       ),
     );
   }
